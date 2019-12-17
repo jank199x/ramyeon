819aef914c4996feba7d315750111badec759e59
Pavel Panichev <papanic@tuta.io>
2019-12-17 14:18:07 +0300

    Moved config to example, added it to gitignore
    also fixed? networking stuff installation

--- steps/360-install-networking.sh
+++ steps/360-install-networking.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-arch-chroot /mnt pacman -S --noconfirm --needed --quiet dhcpcd dialog wpa_supplicant
+arch-chroot /mnt pacman -S --noconfirm --needed --quiet networkmanager
+arch-chroot /mnt systemctl enable NetworkManager
