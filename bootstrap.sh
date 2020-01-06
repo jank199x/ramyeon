3911398e9e471f36a7580933b5f53037a1be47a0
Pavel Panichev <papanic@tuta.io>
2020-01-06 10:24:31 +0000

    Sneaky repo address fix in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ mount -o remount,size=2G /run/archiso/cowspace
 pacman -Sy --noconfirm --needed --quiet git
 
 # Cloning repo
-rm -rf Px201AI && git clone https://gitlab.com/papanic/Px201AI.git
+rm -rf Px201AI && git clone https://gitlab.com/papanic/ramyeon
 
 # Ready to execute
 set +v
